27e4503a037e46c4ae4f3a1e9d47c01269543e09
SebastianSumi <cspam7780@gmail.com>
2025-09-12 16:13:03 -0500

    Spring asistencia

---
+++ PTL_POO/PTL_POO/src/main/java/pe/edu/upeu/demo/Application.java
@@ -0,0 +1,7 @@
+package pe.edu.upeu.demo;
+
+public class Application {
+    public static void main(String[] args) {
+        PtlPooApplication.main(args);
+    }
+}
---
+++ PTL_POO/PTL_POO/src/main/java/pe/edu/upeu/demo/PtlPooApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtlPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtlPooApplication.class, args);
+	}
+
+}
