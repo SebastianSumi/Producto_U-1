2ddd4f6172243b65f8124caf1db3572cd41ad55b
CRISScoD19 <cristiansyl098@gmail.com>
2025-09-18 17:15:05 -0500

    .

--- PTL_POO/PTL_POO/src/main/java/pe/edu/upeu/demo/Application.java
+++ PTL_POO/PTL_POO/src/main/java/pe/edu/upeu/demo/Application.java
@@ -3,5 +3,7 @@ package pe.edu.upeu.demo;
 public class Application {
     public static void main(String[] args) {
         PtlPooApplication.main(args);
+
+        System.out.println("holaaaaaaaa");
     }
 }
